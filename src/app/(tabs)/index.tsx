0b0e7ec7821aced7530615ff6f84fa2cabe12e05
diogopereiraasp <diogopereira5.ds@gmail.com>
2025-10-28 18:17:42 -0300

    feat(routes): adicionando paginas nas tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Home from "../screens/Home";
 
 export default function App() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView>
+            <Home />
+        </SafeAreaView>
     );
 }
