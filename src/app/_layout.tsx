5d38fce2b355fafd4a28739bb703144b636dc70e
diogopereiraasp <diogopereira5.ds@gmail.com>
2025-10-30 11:18:03 -0300

    refactor(details): botão de voltar no header da tela de detalhes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,22 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { colors } from '../styles/theme/colors';
 
 export default function RootLayout() {
+
+    const router = useRouter();
+
     return (
         <SafeAreaProvider>
             <Stack>
                 <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="Details" options={{ headerShown: false }} />
+                <Stack.Screen name="Details" options={{
+                    headerTransparent: true,
+                    headerTitle: '',
+                    headerTintColor: colors.textPrimary,
+                    headerBackTitle: "",
+                    headerBackButtonDisplayMode: 'minimal',
+                }} />
             </Stack>
         </SafeAreaProvider>
     );
