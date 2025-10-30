56fe73b620bddb3569c129fc26fe41b77a997e70
diogopereiraasp <diogopereira5.ds@gmail.com>
2025-10-30 15:38:02 -0300

    feat(favorite): acessando tela de sobre do filme na lista de favoritos

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -5,9 +5,14 @@ import MovieCard from '../../components/MovieCard';
 import { ButtonTrash, Container, Header, Title } from './styles';
 import { Feather } from '@expo/vector-icons';
 import { colors } from '../../styles/theme/colors';
+import { useRouter } from 'expo-router';
 
 export default function FavoritesScreen() {
+
+    const router = useRouter();
     const favorites = useSelector(selectFavorites);
+
+
     return (
         <Container>
 
@@ -21,7 +26,7 @@ export default function FavoritesScreen() {
             <FlatList
                 data={favorites}
                 keyExtractor={(i) => String(i.id)}
-                renderItem={({ item }) => <MovieCard movie={item} />}
+                renderItem={({ item }) => <MovieCard movie={item} onPress={() => router.push({ pathname: "details", params: { id: item.id } })} />}
                 contentContainerStyle={{ padding: 16 }}
                 ListEmptyComponent={null}
             />
